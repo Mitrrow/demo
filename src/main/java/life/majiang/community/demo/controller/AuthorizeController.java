aa6b2cae00bf77edd523af0adcc1e3fbd353d5df
Mitrrow <1571050780@qq.com>
2022-03-16 21:40:27 +0800

    add check cookies

--- src/main/java/life/majiang/community/demo/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/demo/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.majiang.community.demo.controller;
 
 import life.majiang.community.demo.dto.AccessTokenDTO;
 import life.majiang.community.demo.dto.GithubUser;
+import life.majiang.community.demo.mapper.UserMapper;
+import life.majiang.community.demo.model.User;
 import life.majiang.community.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 
 @Controller
@@ -25,12 +31,16 @@ public class AuthorizeController {
     @Value("${github.client.secret}")
     private String clientSecret;
 
+    @Autowired
+    private UserMapper userMapper;
 
 
     @GetMapping("/callback")
 
-    public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
 
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
@@ -40,8 +50,24 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.getState();
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.getAccountId(String.valueOf(githubUser.getId()));
+            user.getGmtCreate(System.currentTimeMillis());
+            user.getGmtModified(user.getGmtCreate());
+
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        } else {
+
+            return "redirect:/";
+        }
+
     }
 }
---
+++ src/main/java/life/majiang/community/demo/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package life.majiang.community.demo.mapper;
+
+
+import life.majiang.community.demo.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into User(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    @Select("select * from User where token= #{token}")
+    User findByToken(@Param("token") String token);
+}
